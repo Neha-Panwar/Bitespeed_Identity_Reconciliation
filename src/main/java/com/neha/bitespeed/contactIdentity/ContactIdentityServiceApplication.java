8882036584a4eefdb7c8ab405b66d2b73e73de77
Neha-Panwar <nehapanwar26198@gmail.com>
2023-06-20 16:50:42 +0530

    initial commit

---
+++ src/main/java/com/neha/bitespeed/contactIdentity/ContactIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.neha.bitespeed.contactIdentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactIdentityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neha/bitespeed/contactIdentity/ContactIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neha.bitespeed.contactIdentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
